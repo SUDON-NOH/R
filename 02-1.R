deb970587f9949c6272fad245658519dd325603d
SUDON-NOH <51067105+SUDON-NOH@users.noreply.github.com>
2019-06-17 10:54:42 +0900

    Update 02-1.R

--- 02-1.R
+++ 02-1.R
@@ -13,3 +13,16 @@ result <- add(50,20)
 print(result)
 result <- add(-100,40)
 print(result)
+
+# 단축키
+# (1) ctrl + enter : 명령어 입력
+# (2) Alt + - : 화살표 입력
+# (3) # : 단일 주석 처리
+# (4) Ctrl + Shift + C : 여러줄 주석 처리
+# (5) Enter : 다음 줄로 이동
+# (6) Console : Ctrl + L : 콘솔 화면 지우기
+# (7) Console : 방향키(위/아래) : 그동안 입력했던 명령어를 확인 할 수 있다
+# (8) Alt + Shift + 방향키(위/아래) : 복사해서 붙여넣기
+# (9) Ctrl + Shift + N : 새로운 스크립트 만들기
+# (10) Ctrl + A : 전체 선택
+# (11) Ctrl + D : 해당 커서 줄 삭제
